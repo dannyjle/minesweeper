2e24c3d42e228360611965419960477328b20b1f
Danny Le <le.dannyj@gmail.com>
2021-05-25 14:23:40 -0400

    Grid created and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = {
+    data: [
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+    ],
+  }
+
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <div>
+        <h1>Minesweeper!</h1>
+        <ul>
+          {this.state.data.map(function (row) {
+            return row.map(function (cell, columnIndex) {
+              return <li key={columnIndex}>{cell}</li>
+            })
+          })}
+        </ul>
+      </div>
+    )
   }
 }
